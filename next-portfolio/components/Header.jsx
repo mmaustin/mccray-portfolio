093a70019cf19955cf027de9b738a80453378389
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-13 13:28:51 -0400

    Crated a navbar component and contact page route folder

--- next-portfolio/components/Header.jsx
+++ next-portfolio/components/Header.jsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h2 className="text-4xl font-semibold">
             McCray<span className="text-accent">.</span>
           </h2>
         </Link>
+        <div className="hidden lg:flex items-center gap-8">
+          <Navbar />
+          <Link href="/contact">
+            <button className="">Contact</button>
+          </Link>
+        </div>
       </div>
     </header>
   )
