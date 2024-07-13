e8220eba361e1199731459e6a9e68c45b61cc04b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-13 16:16:53 -0400

    Built out mobilenav component

--- next-portfolio/components/MobileNav.jsx
+++ next-portfolio/components/MobileNav.jsx
@@ -29,7 +29,27 @@ const MobileNav = () => {
   const pathName = usePathname();
 
   return (
-    <div>MobileNav</div>
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h2 className="text-4xl font-semibold">
+              McCray<span className="text-accent">.</span>
+            </h2>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, i) => {
+            return (
+              <Link key={i} href={link.path} className={`${link.path === pathName && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
+            )
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
   )
 }
 export default MobileNav;
