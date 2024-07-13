bf9f1bb471a5c216879cbce87ea6b9ab94d05e93
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-13 13:01:00 -0400

    Added homepage link to header h2 element

--- next-portfolio/components/Header.jsx
+++ next-portfolio/components/Header.jsx
@@ -1,9 +1,18 @@
-
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        <Link href="/">
+          <h2 className="text-4xl font-semibold">
+            McCray<span className="text-accent">.</span>
+          </h2>
+        </Link>
+      </div>
+    </header>
   )
 }
 export default Header;
