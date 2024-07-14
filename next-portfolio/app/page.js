f1001c0155518567f4fcfedd42c888b2270f8a33
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-14 13:25:18 -0400

    Created social media component to handle links to media sites

--- next-portfolio/app/page.js
+++ next-portfolio/app/page.js
@@ -1,3 +1,4 @@
+import SocialMedia from "@/components/SocialMedia";
 import { Button } from "@/components/ui/button";
 import { Fullscreen } from "lucide-react";
 import { FiDownload } from "react-icons/fi"
@@ -8,10 +9,18 @@ const HomePage = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-24">
           <div className="text-center lg:text-left">
-            <span>CODER</span>
-            <h1 className="h1">Hello, I'm <br />
-              <span>McCray Austin.</span>
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">Hello, I'm <br />
+              <span className="text-accent">McCray Austin.</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Think of something interesting to say about myself right here. Just a few more words will do.  Make it pithy!
+            </p>
+            <div className="flex flex-col lg:flex-row items-center gap-8">
+              <div className="mb-8 lg:mb-0">
+                <SocialMedia />
+              </div>
+            </div>
           </div>
 
 
---
+++ next-portfolio/components/SocialMedia.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+
+
+const SocialMedia = () => {
+  return (
+    <div>Social Media</div>
+  )
+}
+export default SocialMedia
