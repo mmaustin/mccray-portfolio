d72dd68b583fc02042f89d423d0fe33b2b30df4c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-14 13:50:41 -0400

    Experimenting with proper way to use next link for external sites

--- next-portfolio/components/SocialMedia.jsx
+++ next-portfolio/components/SocialMedia.jsx
@@ -1,10 +1,19 @@
-import Link from "next/link"
-import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/mmaustin" },
+  { icon: <FaLinkedinIn />, path: "https://linkedin.com/in/mccray-austin" }
+]
 
 
-const SocialMedia = () => {
+const SocialMedia = ({ containerStyles, iconStyles }) => {
   return (
-    <div>Social Media</div>
+    <div className={containerStyles}>{
+      socials.map((media, i) => {
+        return <a href={media.path} key={i} className={iconStyles} target="_blank" >{media.icon}</a>
+      })
+    }</div>
   )
 }
 export default SocialMedia
