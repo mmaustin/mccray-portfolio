3d7397b15b6c4c741731505bab14cb95af2437a7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-17 07:21:38 -0400

    crated an 'about me' object on about page

--- next-portfolio/app/about/page.jsx
+++ next-portfolio/app/about/page.jsx
@@ -1,8 +1,31 @@
+'use client';
 
+const about = {
+  title: "About Me",
+  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptas nam expedita dolorum delectus voluptatum, dolores illum, excepturi eligendi dignissimos, hic quisquam quis distinctio mollitia officia dolor earum harum ea.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "McCray Austin"
+    },
+    {
+      fieldName: "Location",
+      fieldValue: "Brooklyn, NY"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "mccrayaustin@ymail.com"
+    },
+    {
+      fieldName: "Telephone",
+      fieldValue: "973-634-0148"
+    },
+  ],
+}
 
 const About = () => {
   return (
-    <div className="">About</div>
+    <div className="">lorem</div>
   )
 }
 export default About;
