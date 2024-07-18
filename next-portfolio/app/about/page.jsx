977f288d3418e1f627e571cb1e101f29d9975a49
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-18 09:37:52 -0400

    Completed styling of skills section

--- next-portfolio/app/about/page.jsx
+++ next-portfolio/app/about/page.jsx
@@ -229,14 +229,25 @@ const About = () => {
 
             <TabsContent value="skills" className="w-full h-full" >
               <div className="flex flex-col gap-[30px]">
-                <div className="">
+                <div className="flex flex-col gap-[30px] text-center lg:text-left">
                   <h3 className="text-4xl font-bold">{skills.title}</h3>
-                  <p>{skills.description}</p>
+                  <p className="max-w-[600px] text-white/60 mx-auto lg:mx-0">{skills.description}</p>
                 </div>
-                <ul>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
                   {skills.skillList.map((skill, i) => {
                     return (
-                      <li key={i}>{skill.name}</li>
+                      <li key={i}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-4xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize">{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
                     )
                   })}
                 </ul>
