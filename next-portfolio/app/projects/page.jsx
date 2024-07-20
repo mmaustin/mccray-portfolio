eeeb2d70d2e2633cfbfcdbb433d6f181d21b346b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-20 13:18:28 -0400

    Implemented swiper, troubleshooting issues with swiper

--- next-portfolio/app/projects/page.jsx
+++ next-portfolio/app/projects/page.jsx
@@ -20,8 +20,8 @@ const projects = [
       { name: "Daisy UI" }
     ],
     image: '/novel-ideas.png',
-    live: "",
-    github: "",
+    live: "https://project-fb.vercel.app",
+    github: "https://github.com/mmaustin/project-fb",
   },
   {
     num: "02",
@@ -34,8 +34,8 @@ const projects = [
       { name: "JWT" }
     ],
     image: '/writers-forum.png',
-    live: "",
-    github: "",
+    live: "https://writers-forum.onrender.com",
+    github: "https://github.com/mmaustin/writers-forum",
   },
   {
     num: "03",
@@ -48,8 +48,8 @@ const projects = [
       { name: "Material UI" }
     ],
     image: '/novel-ideas.png',
-    live: "",
-    github: "",
+    live: "https://random-history.onrender.com",
+    github: "https://github.com/mmaustin/random-history",
   },
 ];
 
@@ -57,25 +57,32 @@ const Projects = () => {
 
   const [project, setProject] = useState(projects[0]);
 
+  const handleSlideChange = swiper => {
+    console.log(swiper.activeIndex, projects.length);
+    const currentIndex = swiper.activeIndex;
+
+    setProject(projects[currentIndex]);
+  }
+
   return (
     <div className="min-h-[80vh] flex flex-col justify-center py-12 lg:px-0">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row lg:gap-[30px">
           <div className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2 lg:order-none">
-            <div className="">
-              <div className="text-6xl leading-none text-white/50">{project.num}</div>
-              <h2 className="text-[42px] font-semibold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
-              <p className="text-white/50">{project.description}</p>
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-6xl leading-none text-white/50">{project?.num}</div>
+              <h2 className="text-[42px] font-semibold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project?.category} project</h2>
+              <p className="text-white/50">{project?.description}</p>
               <ul className="flex gap-4">
-                {project.stack.map((item, i) => {
+                {project?.stack.map((item, i) => {
                   return (
                     <li key={i} className="text-lg text-accent">{item.name}</li>
                   )
                 })}
               </ul>
               <div className="border border-white"></div>
-              <div className="">
-                <Link href={project.live}>
+              <div className="flex items-center gap-4">
+                <Link href={project?.live} target="_blank">
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -87,10 +94,33 @@ const Projects = () => {
                     </Tooltip>
                   </TooltipProvider>
                 </Link>
+
+                <Link href={project?.github} target="_blank">
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repo</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="w-full">slider</div>
+          <div className="w-full lg:w-[50%]">
+            <Swiper spaceBetween={10} slidesPerView={1} className="lg:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+              {projects.map((project, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    {project.title}
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </div>
--- next-portfolio/components/Footer.jsx
+++ next-portfolio/components/Footer.jsx
@@ -3,7 +3,7 @@
 
 const Footer = () => {
   return (
-    <footer className="py-8 xl:py-12 text-white lg:h-full flex justify-center items-center">
+    <footer className="py-8 xl:py-12 text-white lg:h-full flex justify-center items-center mt-10">
       <div className="container mx-auto flex justify-between items-center">
         Footer Stuff
       </div>
