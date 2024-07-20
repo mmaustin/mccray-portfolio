1ba69275cf6741fa270b4fbfec33bc9ea3e3c6aa
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-20 11:15:04 -0400

    Added first prelimenary stylings to project elements

--- next-portfolio/app/projects/page.jsx
+++ next-portfolio/app/projects/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Swiper, SwiperSlide, } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
@@ -54,8 +54,23 @@ const projects = [
 ];
 
 const Projects = () => {
+
+  const [project, setProject] = useState(projects[0]);
+
   return (
-    <div>Projects</div>
+    <div className="min-h-[80vh] flex flex-col justify-center py-12 lg:px-0">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-[30px">
+          <div className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2 lg:order-none">
+            <div className="">
+              <div className="text-6xl leading-none ">{project.num}</div>
+              <h2 className="text-[42px] font-semibold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+            </div>
+          </div>
+          <div className="w-full">slider</div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Projects;
