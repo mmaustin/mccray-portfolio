a5d779d5a61be81a39f8e328b5c4eaed389d9646
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-07-22 13:17:12 -0400

    Removed social media component from homepage and place it in the footer

--- next-portfolio/components/Footer.jsx
+++ next-portfolio/components/Footer.jsx
@@ -1,11 +1,14 @@
-
-
+import SocialMedia from "@/components/SocialMedia";
 
 const Footer = () => {
   return (
-    <footer className="py-8 xl:py-12 text-white lg:h-full flex justify-center items-center mt-10">
+    <footer className="py-8 xl:py-12 text-white bg-[#27272c] lg:h-full flex justify-center items-center mt-10">
       <div className="container mx-auto flex justify-between items-center">
-        Footer Stuff
+        <div className="flex flex-col lg:flex-row items-center gap-8">
+          <div className="mb-8 lg:mb-0">
+            <SocialMedia containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+          </div>
+        </div>
       </div>
     </footer>
   )
