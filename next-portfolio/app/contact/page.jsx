329eecd22024b4ce5008c74a7e96f0cd85a4c864
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-05 14:49:19 -0400

    Created a phone link component

--- next-portfolio/app/contact/page.jsx
+++ next-portfolio/app/contact/page.jsx
@@ -1,13 +1,13 @@
-import EmailLink from "@/components/Email";
+import EmailLink from "@/components/EmailLink";
+import PhoneLink from "@/components/PhoneLink";
 import Photo from "@/components/Photo";
-import SocialMedia from "@/components/SocialMedia";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 
 const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
-    description: "973-634-0148"
+    description: <PhoneLink />
   },
   {
     icon: <FaEnvelope />,
@@ -32,33 +32,25 @@ const ContactPage = () => {
                 {info.map((item, i) => {
                   return (
                     <li key={i} className="flex items-center gap-6">
-                      <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="w-[52px] h-[52px] lg:w-[72px] lg:h-[72px] bg-[#27272c] text-accent/50 rounded-md flex items-center justify-center">
                         <div className="text-[28px]">
                           {item.icon}
                         </div>
                       </div>
                       <div className="flex-1">
-                        <p className="text-white/60">{item.title}</p>
-                        <h3 className="text-xl">{item.description}</h3>
+                        <p className="text-white/70">{item.title}</p>
+                        <h3 className="text-xl text-accent">{item.description}</h3>
                       </div>
                     </li>
                   )
                 })}
               </ul>
-              {/* <div className="w-auto">
-                <div className="flex flex-col lg:flex-row items-center gap-8">
-                  <div className="mb-8 lg:mb-0 mt-6">
-                    <SocialMedia containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
-                  </div>
-                </div>
-              </div> */}
             </div>
           </div>
           <div className=" mb-8 lg:mb-0 rounded-xl">
             <Photo />
           </div>
         </div>
-        {/* <p><a href="mailto:mccrayaustin@ymail.com">Send email</a></p> */}
       </div>
     </section>
   )
--- next-portfolio/components/Email.jsx
+++
@@ -1,9 +0,0 @@
-
-
-
-const EmailLink = () => {
-  return (
-    <p className="text-accent/50"><a href="mailto:mccrayaustin@ymail.com">Email Me!</a></p>
-  )
-}
-export default EmailLink;
