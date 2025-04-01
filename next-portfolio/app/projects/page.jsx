f28fc851c922d3af4c67ffa1abe3a7f89f117bb3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-01 17:49:36 -0400

    Changing out project pictures

--- next-portfolio/app/projects/page.jsx
+++ next-portfolio/app/projects/page.jsx
@@ -8,9 +8,9 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import Link from "next/link";
 import Image from "next/image";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
-import novelIdeas from "@/public/novel-ideas-three.png";
-import wfBig from "@/public/wf-big.png";
-import randomHistory from "@/public/random-history-two.png";
+import novelIdeas from "@/public/work-form.png";
+import wfBig from "@/public/job-card-2.png";
+import randomHistory from "@/public/manifest-8.png";
 
 const projects = [
   {
@@ -70,7 +70,7 @@ const Projects = () => {
   return (
     <div className="min-h-[80vh] flex flex-col justify-center py-12 lg:px-0">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row lg:gap-[30px]">
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center lg:gap-[70px]">
           <div className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2 lg:order-none">
             <div className="flex flex-col gap-[30px] h-[50%] text-center">
               <div className="text-6xl leading-none text-white/50">{project?.num}</div>
@@ -113,26 +113,34 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="my-5 w-full lg:w-[50%]">
-            <Swiper spaceBetween={10} slidesPerView={1} className="lg:h-[420px] mb-12" onSlideChange={handleSlideChange}>
+          <div className="my-5 w-[350px] h-[250px]">
+            <Swiper spaceBetween={10} slidesPerView={1} className="lg:h-[250px] mb-12" onSlideChange={handleSlideChange}>
               {projects.map((project, i) => {
                 return (
                   <SwiperSlide key={i} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    {/* <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20 border border-red-500"> */}
 
-                      <div className="absolute top-0 bottom-0 w-full bg-black/10 z-20"></div>
+                    {/* <div className="absolute top-0 bottom-0 w-full bg-black/10 z-20 border border-red-400"></div> */}
 
-                      <div className="relative w-full h-full">
-                        <Image
-                          src={project.image}
-                          fill
-                          priority
-                          className="object-cover"
-                          sizes="100vw"
-                          alt={project.title}
-                        />
-                      </div>
-                    </div>
+                    {/* <div className="relative w-full h-full"> */}
+                    {/* <Image
+                            src={project.image}
+                            fill
+                            priority
+                            className="object-cover"
+                            sizes="100vw"
+                            alt={project.title}
+                          /> */}
+                    <Image
+                      className="h-[250px] w-[350px]"
+                      src={project.image}
+                      alt={project.title}
+                      priority
+                      height={265}
+                      width={198}
+                    />
+                    {/* </div> */}
+                    {/* </div> */}
                   </SwiperSlide>
                 )
               })}
