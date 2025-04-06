b34ef8d4442ca60282328ad785c4aa284f40b67d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-06 17:18:58 -0400

    Removed scroll area component from mobile nav component

--- next-portfolio/components/MobileNav.jsx
+++ next-portfolio/components/MobileNav.jsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription, SheetClose } from "./ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
-import { ScrollArea } from "./ui/scroll-area";
+import { cn } from "@/lib/utils";
 
 
 const links = [
@@ -36,11 +36,11 @@ const MobileNav = () => {
   const pathName = usePathname();
 
   return (
-    <Sheet>
+    <Sheet className={cn('bg-slate-300')}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
-      <SheetContent className="flex items-center flex-col mt-20">
+      <SheetContent className="flex items-center flex-col mt-0 border">
         <SheetTitle >
           <VisuallyHidden.Root>
             Mobile Menu Title
@@ -51,7 +51,14 @@ const MobileNav = () => {
             Mobile Menu Description
           </VisuallyHidden.Root>
         </SheetDescription>
-        <ScrollArea className="h-32 w-48 rounded-md border border-accent mt-10">
+        {links.map((link, i) => {
+          return (
+            <SheetClose key={i} asChild>
+              <Link key={i} href={link.path} className={`${link.path === pathName && "text-accent border-b-2 border-accent text-lg"} m-0 text-base capitalize hover:text-accent transition-all`}>{link.name}</Link>
+            </SheetClose>
+          )
+        })}
+        {/* <ScrollArea className="h-32 w-48 rounded-md border border-accent mt-10">
           <nav className="mt-2 flex flex-col justify-center items-center gap-8">
             {links.map((link, i) => {
               return (
@@ -61,7 +68,7 @@ const MobileNav = () => {
               )
             })}
           </nav>
-        </ScrollArea>
+        </ScrollArea> */}
       </SheetContent>
     </Sheet>
   )
