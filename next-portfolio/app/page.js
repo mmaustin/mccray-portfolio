5614d4cf22ec4d6c44701cfd55a4cbf21637fb6a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-18 10:15:26 -0400

    Reorganized job description and name on homepage

--- next-portfolio/app/page.js
+++ next-portfolio/app/page.js
@@ -7,10 +7,10 @@ const HomePage = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-24">
           <div className="text-center lg:text-left order-2 lg:order-none">
-            <span className="text-xl">Software Developer</span>
-            <h1 className="h1 mb-6">Hello, I'm <br />
-              <span className="text-accent">McCray Austin.</span>
+            <h1 className="h1 mb-4">
+              McCray Austin
             </h1>
+            <p className="text-4xl text-accent mb-4">Web Developer</p>
             <p className="max-w-[500px] mb-9 text-white/80">
               I love to code, create, and collaborate with dynamic individuals and institutions.  Let's excel together!
             </p>
