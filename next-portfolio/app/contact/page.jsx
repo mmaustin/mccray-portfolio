df9822cd2ee43ce17c12251654f392de6e881f6e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-18 13:39:11 -0400

    Changed text color of desired job, contact page

--- next-portfolio/app/contact/page.jsx
+++ next-portfolio/app/contact/page.jsx
@@ -7,7 +7,7 @@ import { IoCodeSlashSharp } from "react-icons/io5";
 const info = [
   {
     icon: <IoCodeSlashSharp />,
-    title: <p className=" text-3xl">Software Engineer</p>,
+    title: <p className="text-accent text-3xl">Software Engineer</p>,
     description: ""
   },
   {
